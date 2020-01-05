3b51f11eb18b750d6b570893fd06a06417cd48cf
Charles Iliya Krempeaux <iamreiver@gmail.com>
2020-01-04 22:39:24 -0800

    docs

---
+++ src/cyber80/rgb/doc.go
@@ -0,0 +1,28 @@
+/*
+Package cyber80_rgb provides tools working with 3-byte — i.e., 24-bit — RGB colors.
+
+Creation
+
+One way of creating a cyber80_rgb.Type is:
+
+	red   :=  41 // == 0x29
+	green := 173 // == 0xad
+	blue  := 255 // == 0xff
+	
+	rgb := cyber80_rgb.RGB(red, green, blue)
+
+Another way of creatig a cyber80_rgb.Type is:
+
+	var colors []byte = []bytes{
+		0x29,0xad,0xff, // blue
+		0x83,0x76,0x9c, // indigo
+		0xff,0x77,0xab, // pink
+		0xff,0xcc,0xaa, // peach
+	}
+	
+	p := buffer[3]
+	
+	rgb := cyber80_rgb.Bytes(p)
+
+*/
+package cyber80_rgb
