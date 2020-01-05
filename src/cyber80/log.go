89796690f8a56739126aa15cbbbdb08f84325493
Charles Iliya Krempeaux <iamreiver@gmail.com>
2020-01-04 22:23:40 -0800

    docs

--- src/cyber80/log.go
+++ src/cyber80/log.go
@@ -5,6 +5,14 @@ import (
 	"strings"
 )
 
+// log outputs a log message.
+//
+// If cyber80 is run inside a web browser then the log will appear in the web browser's developer console.
+// I.e., this is similar to JavaScript's console.log().
+//
+// Example
+//
+//	log("Hello world!")
 func log(a ...interface{}) {
 
 	var builder strings.Builder
@@ -16,6 +24,16 @@ func log(a ...interface{}) {
 	fmt.Print(builder.String())
 }
 
+// logf outputs a formatted log message.
+//
+// If cyber80 is run inside a web browser then the log will appear in the web browser's developer console.
+// I.e., this is similar to JavaScript's console.log().
+//
+// Example
+//
+//	var name = "Joe Blow"
+//
+//	logf("Hello %s!", name)
 func logf(format string, a ...interface{}) {
 
 	var builder strings.Builder
