43f019f9e375f26a29873cbbf823e827c9534375
Charles Iliya Krempeaux <iamreiver@gmail.com>
2020-01-04 16:24:10 -0800

    cyber80_palette

---
+++ src/cyber80/palette/consts.go
@@ -0,0 +1,16 @@
+package cyber80_palette
+
+const (
+	// colorSize represents the number of bytes per color.
+	colorSize = 3
+)
+
+const (
+	// Count represents the number of colors in a palette.
+	Count = 16
+)
+
+const (
+	// Size represents the size of the palette in bytes.
+	Size = colorSize * Count
+)
--- src/cyber80/palette/size.go
+++
@@ -1,9 +0,0 @@
-package cyber80_palette
-
-const (
-	colorSize = 3
-)
-
-const (
-	Size = colorSize * 16
-)
