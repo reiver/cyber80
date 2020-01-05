8dd130468961547e172acffa4a507f7fa1d55563
Charles Iliya Krempeaux <iamreiver@gmail.com>
2020-01-04 23:14:42 -0800

    docs

--- src/cyber80/ram/doc.go
+++ src/cyber80/ram/doc.go
@@ -21,9 +21,34 @@ And, for example, to get the value at memort ‘1015’ one could do something s
 
 Size
 
-The size of cyber80_ram.Value (and all cyber80_ram.Type), in bytes, is given by:
+The total size of cyber80_ram.Value (and all cyber80_ram.Type), in bytes, is given by:
 
 	cyber80_ram.Size
 
+Raster Display Memory
+
+The raster display memory starts at memory address:
+
+	cyber80_ram.BPRaster
+
+The last byte of raster display memory is at memory address:
+
+	cyber80_ram.BPRaster + cyber80_raster.Size - 1
+
+To write at the 5th memory location of the raster display memory one could do something similar to:
+
+	index := cyber80_ram.BPRaster + 5 - 1
+	
+	cyber80_ram.Value[index] = 0x0b
+
+Although, since most people want to think is terms of (x,y) coordinates when dealing with pixels the raster display memory,
+one could use the cyber80_raster.XY() helper. For example:
+
+	x := 8
+	y : 16
+	
+	index := cyber80_raster.XY(x, y)
+	
+	cyber80_ram.Value[index] = 0x0b
 */
 package cyber80_ram
